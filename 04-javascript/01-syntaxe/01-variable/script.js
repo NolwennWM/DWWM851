df96c27017e66bb240c17aab4fa65629d65d64dd
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-10-02 10:22:00 +0200

    cours syntaxe js

--- 04-javascript/01-syntaxe/01-variable/script.js
+++ 04-javascript/01-syntaxe/01-variable/script.js
@@ -326,3 +326,112 @@ console.log(a2, ...a2);
 a4 = [...a2];
 a4.push("coucou");
 console.log(a2, a4);
+// On peut aussi se servir du spread operator pour fusionner un tableau dans un nouveau tableau.
+let a5 = ["pizza", ...a4, "pomme", "banane"];
+console.log(a5);
+// Sans le spread operator, on se retrouve avec un tableau multi dimensionnel (Un tableau dans un tableau)
+let a6 = ["pizza", a4, "pomme", "banane"];
+console.log(a6);
+// Pour accèder à une valeur d'un tableau multi dimensionnel, on selectionnera d'abbord celui ci avant d'ajouter l'index voulu dans le second tableau :
+console.log(a6[1][2]);
+// On peut avoir autant de tableau dans un tableau que l'on souhaite :
+let a7 = [[[[[["Coucou"]]]]]];
+console.log(a7, a7[0][0][0][0][0][0]);
+
+// Pour échanger la position de deux valeurs dans un tableau, on pourra évidement utiliser une variable temporaire.
+console.log("a3",a3);
+let tmp = a3[0];
+a3[0] = a3[3];
+a3[3] = tmp;
+console.log("a3",a3);
+// Mais javascript permet aussi de le faire ainsi :
+[a3[0], a3[1]] = [a3[1], a3[0]];
+console.log("a3",a3);
+
+// ? -------------- objets ---------------------
+/* 
+    Les objets JS ressemblent aux tableaux, mais utilisent "{}" au lieu de "[]".
+    Et au lieu d'être indexé numériquement, ils sont indexé par des string qui servent de nom de propriété.
+    On pourra par exemple dire que la propriété "nom" vaudra "Dupont"
+*/
+let o1 = {nom: "Dupont", age: 45, loisir: ["bowling", "mahjong"]};
+console.log(o1);
+// Pour accèder à une propriété d'un objet, je peux le faire comme avec un tableau :
+console.log(o1["nom"], o1["age"], o1["loisir"]);
+// Mais les objets peuvent aussi être utilisé ainsi :
+console.log(o1.nom, o1.age, o1.loisir);
+// ici dans le premier cas il va chercher la valeur de "v" et dans le second, il cherche une propriété "v".
+let v = "age";
+console.log(o1[v], o1.v);
+// Si on veut accèder à un tableau dans un objet, il suffit de le selectionner :
+console.log(o1.loisir[0], o1["loisir"][0]);
+
+// On peut avoir autant d'objet dans un objet que voulu.
+let o2 = {vegetal: {legume : {haricot: {couleur: "vert"}}}};
+console.log(o2.vegetal.legume.haricot.couleur);
+
+// Comme pour les tableaux, on ne peut pas copier sans modifier l'original.
+let o3 = o1;
+o3.signe = "balance";
+console.log(o1, o3);
+// On devra aussi utiliser le spread operator:
+let o4 = {...o1};
+o4.signe = "scorpion";
+console.log(o1, o4);
+
+// On peut aussi se servir du spread operator pour transformer un tableau en objet :
+console.log(a1, {...a1});
+
+o4.yeux = "vert";
+/* Dans le cas d'une fusion d'objet, les propriétés uniques seront ajouté, et celles en commun, se verront remplacé par la dernière */
+let o5 = {...o4, ...o1};
+console.log(o5);
+
+// même dans un const, un tableau ou un objet est modifiable :
+const a0 = [], o0 = {};
+a0[0] = "coucou";
+o0.text = "coucou";
+// a0 = "test";
+console.log(a0, o0);
+
+// ? ---------------- Les booleans -------------
+// Les booleans n'acceptent que deux valeurs, soit "true", soit "false";
+let b1 = true, b2 = false;
+
+// On peut faire apparaitre des booleans de tout un tas de façon :
+// Plus petit ou plus grand :
+console.log("1 < 2 :", 1 < 2);
+console.log("1 > 2 :", 1 > 2);
+// Plus petit ou égale, plus grand ou égale :
+console.log("1 <= 2 :", 1 <= 2);
+console.log("1 >= 2 :", 1 >= 2);
+// Vérifier une égalité :
+console.log("1 == 1 :", 1 == 1);
+console.log("1 == '1' :", 1 == '1');
+// Vérifier une égalité stricte :
+console.log("1 === 1 :", 1 === 1);
+console.log("1 === '1' :", 1 === '1');
+// Vérifier la différence, ou la différence stricte :
+console.log("1 != '1' :", 1 != '1');
+console.log("1 !== '1' :", 1 !== '1');
+
+// On peut inverser un résultat en le faisant précéder d'un "!" :
+console.log("b1 :", b1, "b2 :", b2);
+console.log("!b1 :", !b1, "!b2 :", !b2);
+
+// On peut regroupper les booleans avec les opérateurs "et" et "ou" représenté par les symboles "&&" et "||" :
+console.log(true && true, true && false, false && true, false && false);
+console.log(true || true, true || false, false || true, false || false);
+
+// On peut ajouter des parenthèses à tout cela :
+console.log(true && (false || true));
+
+// On peut "court-circuiter" un code avec ces comparateurs :
+let nb = 0;
+// dans le cas d'un &&, si le premier est faux, le second ne sera pas vérifié:
+console.log(true && ++nb, nb);
+console.log(false && ++nb, nb);
+// Dans le cas d'un ||, si le premier est vrai, le second ne sera pas vérifié.
+console.log(false || ++nb, nb);
+console.log(true || ++nb, nb);
+
