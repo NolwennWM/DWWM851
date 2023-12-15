67955c6ea0afa99118d81bb678e8e19eab2fb487
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-12-15 09:21:33 +0100

    config et connexion

--- 07-php/ressources/config/_blogConfig.php
+++ 07-php/ressources/config/_blogConfig.php
@@ -1,1 +1,30 @@
-<?php ?>
+<?php 
+// var_dump($_ENV);
+return [
+    // L'host est l'hebergeur de la BDD
+    "host"=>$_ENV["DB_HOST"]??"127.0.0.1",
+    // le port utilisé pour se connecter
+    "port"=>$_ENV["DB_PORT"]??"3306",
+    // Le nom de la BDD
+    "database"=>$_ENV["DB_NAME"]??"blog",
+    // le nom d'utilisateur pour se connecter
+    "user"=>$_ENV["DB_USER"]??"root",
+    // le mot de passe 
+    "password"=>$_ENV["DB_PASSWORD"]??"root",
+    // Le set de caractère utilisé 
+    "charset"=>$_ENV["DB_CHARSET"]??"utf8mb4",
+    /* 
+        Ce tableau d'option sera utilisé par "PDO" 
+        PHP Data Object est un objet servant à la connexion aux BDD.
+    */
+    "options"=>
+    [
+        // Choisir le mode d'affichage d'erreur
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        // Choisir le mode de récupération des données (ici tableau associatif)
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        // J'indiquer à PDO de ne pas émuler les requêtes préparer, ce sera ma BDD qui s'en occupera
+        PDO::ATTR_EMULATE_PREPARES => false
+    ]
+];
+?>
--- 07-php/ressources/services/_pdo.php
+++ 07-php/ressources/services/_pdo.php
@@ -1,1 +1,38 @@
-<?php ?>
+<?php 
+
+function connexionPDO(): \PDO
+{
+    // Je récupère les informations du fichier de configuration.
+    $config = require __DIR__ . "/../config/_blogConfig.php";
+    /* 
+        Je vais devoir constuire un DSN, "Data Source Name" 
+        C'est un string qui contient toute les informations pour localiser la BDD. 
+        il prendra la forme suivante :
+            "pilote de la bdd":host="hôte de la bdd";port="port de la bdd";dbname="nom de la bdd";charset="charset de la bdd";
+        Par exemple :
+            mysql:host=localhost;port=3306;dbname=blog;charset=utf8mb4
+    */
+    $dsn =  "mysql:host=".$config["host"]
+            .";port=". $config["port"]
+            .";dbname=". $config["database"]
+            .";charset=". $config["charset"];
+    try
+    {
+        /* 
+            On crée une nouvelle instance de PDO en lui donnant en argument :
+                Le DSN,
+                Le nom d'utilisateur
+                Le mot de passe
+                Et les options de PDO
+        */
+        $pdo = new \PDO($dsn, $config["user"], $config["password"], $config["options"]);
+        return $pdo;
+    }catch(\PDOException $e)
+    {
+        /* 
+            On lance une nouvelle instance de "PDOException"
+        */
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+}
+?>
